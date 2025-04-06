2e829a8e439973e83da653b01903479ef6cf4342
Ricardo55010 <richi_mari.2011@hotmail.com>
2025-04-05 23:12:01 -0600

    Start: New project Authorization server for spring

---
+++ src/main/java/com/example/StoreAuthorizerServer/StoreAuthorizerServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.StoreAuthorizerServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreAuthorizerServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoreAuthorizerServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/StoreAuthorizerServer/StoreAuthorizerServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.StoreAuthorizerServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreAuthorizerServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
